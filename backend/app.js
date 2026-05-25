16810808a879f2d1fa6ce99f52950a8963da278c
lekkalaprathyusha <prathyushaprathyu1607@gmail.com>
2026-05-26 01:24:10 +0530

    fixed vercel backend

--- backend/app.js
+++ backend/app.js
@@ -1,33 +1,64 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
-const errorHandler = require('./middleware/errorMiddleware')
-require('./database/db')
-
-const authRoutes = require('./routes/authRoutes')
-const slotRoutes = require('./routes/slotRoutes')
-const appointmentRoutes = require('./routes/appointmentRoutes')
-const patientRecordRoutes = require('./routes/patientRecordRoutes')
-const dashboardRoutes = require('./routes/dashboardRoutes')
 
 dotenv.config()
 
+const errorHandler =
+  require('./middleware/errorMiddleware')
+
+require('./database/db')
+
+const authRoutes =
+  require('./routes/authRoutes')
+
+const slotRoutes =
+  require('./routes/slotRoutes')
+
+const appointmentRoutes =
+  require('./routes/appointmentRoutes')
+
+const patientRecordRoutes =
+  require('./routes/patientRecordRoutes')
+
+const dashboardRoutes =
+  require('./routes/dashboardRoutes')
+
 const app = express()
 
+// ==========================
+// MIDDLEWARE
+// ==========================
 app.use(cors())
+
 app.use(express.json())
+
+// ==========================
+// ROUTES
+// ==========================
 app.use('/api/auth', authRoutes)
+
 app.use('/api/doctor-slots', slotRoutes)
+
 app.use('/api/appointments', appointmentRoutes)
+
 app.use('/api/patient-records', patientRecordRoutes)
+
 app.use('/api/dashboard', dashboardRoutes)
+
+// ==========================
+// TEST ROUTE
+// ==========================
 app.get('/', (req, res) => {
-  res.send('Healthcare API Running')
+  res.send('MediSync API Running')
 })
 
+// ==========================
+// ERROR HANDLER
+// ==========================
 app.use(errorHandler)
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
-})
 
+// ==========================
+// EXPORT APP
+// ==========================
+module.exports = app
